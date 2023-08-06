48f64324266f65086fd5a9a7ccc44824831b8f82
gulsah01 <gulsahozkan90@gmail.com>
2023-08-07 00:18:56 +0100

    update

--- src/practice_day07/forEachLoop2.java
+++ src/practice_day07/forEachLoop2.java
@@ -4,7 +4,32 @@ import java.util.Scanner;
 public class forEachLoop2 {
     public static void main(String[] args) {
 
+         /*
+         Bir ArrayList oluşturun ve içine çeşitli ürün fiyatları ekleyin.
+         Bu fiyatların toplamını hesaplayarak ekrana yazdırın.
+         Örnek çıktı:
+         Toplam fiyat: 236.5
+          */
+
+
         ArrayList<Double> fiyatlar=new ArrayList<>();
+        System.out.println("1. urun :");
+        Scanner scanner=new Scanner(System.in);
+        double urun1=scanner.nextDouble();
+        fiyatlar.add(urun1);
+        System.out.println("2. urun :");
+        double urun2=scanner.nextDouble();
+        fiyatlar.add(urun2);
+        System.out.println("3. urun :");
+        double urun3=scanner.nextDouble();
+        fiyatlar.add(urun3);
+
+        double toplam=0;
+        for (double each:fiyatlar
+             ) {
+            toplam+=each;
+        }
+        System.out.println("Urunlerin toplam fiyati: "+ toplam);
 
     }
 }
--- src/practice_day07/forEachLoop3.java
+++ src/practice_day07/forEachLoop3.java
@@ -1,7 +1,27 @@
 package practice_day07;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class forEachLoop3 {
     public static void main(String[] args) {
+// Kullanıcıdan 5 adet not alarak bir ArrayList oluşturun.
+// Bu notların ortalamasını hesaplayarak ekrana yazdırın.
+// Örnek çıktı:
+// Notların ortalaması: 78.5
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen notlari giriniz : ");
+
+        ArrayList<Double> ortalama=new ArrayList();
+
+        for (int i = 0; i <5 ; i++) {
+            System.out.println((i+1)+". dersinizin notu");
+            double notlar = scanner.nextDouble();
+            ortalama.add(notlar);
+
+
+        }
+
+        }
     }
 }
